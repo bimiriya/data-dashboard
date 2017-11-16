149fe4f7dbadc3db9633b3a5b882593bcc2f5814
bimiriya <poyarzunaspee@gmail.com>
2017-11-16 19:57:34 -0300

    branches merged, burger menu done

--- js/app.js
+++ js/app.js
@@ -1,40 +1,16 @@
 var menu = document.getElementById("menu");
-
-//elementos creados para el menu
-var contenedorMenu = document.createElement("div");
-var content = document.createElement("ul");
-var menuName = document.createElement("li");
-var addStudent = document.createElement("li");
-var removeStudent = document.createElement("li");
-var addSprint = document.createElement("li");
-
-var menuContent = addStudent, removeStudent;
-
-//atributos
-contenedorMenu.setAttribute("id","contenedor-menu");
-menuContent.setAttribute("id","menu-content");
-
-var ayy = document.createTextNode("VALENTINA SMITH");
-
-
-
-/*contenido de elementos creados
-menuName.innerHTML = "VALENTINA SMITH";
-addStudent.innerHTML = "Add student";
-removeStudent.innerHTML = "Remove student";
-addSprint.innerHTML = "Add sprint";*/
+var menuDesplegable = document.getElementById("menu-desplegable");
+var menUsername = document.getElementById("menu-username");
 
 menu.onclick = function() {
-    
-    menu.appendChild(contenedorMenu);
-    contenedorMenu.appendChild(content);
-    content.appendChild(menuName);
-    menuName.appendChild(ayy);
-    content.appendChild(addStudent);
-    content.appendChild(removeStudent);
-    content.appendChild(addSprint);
-    
+    menu.style.display = "none";
+    menuDesplegable.style.height = window.innerHeight + "px";
+    menuDesplegable.style.display = "initial";
 }
 
+menUsername.onclick = function() {
+    menuDesplegable.style.display = "none";
+    menu.style.display = "initial";
+}
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
